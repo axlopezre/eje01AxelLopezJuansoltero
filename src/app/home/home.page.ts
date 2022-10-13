0f15f2bdb21ebc2cff42ee03eb490bcf0f74ca96
axlopezre <83840788+axlopezre@users.noreply.github.com>
2022-10-13 15:01:24 -0600

    Proyecto Imagenes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public photos= [];
 
-  constructor() {}
+
+  constructor() {
+    this.photos =[
+      {
+          name:"Foto 1",
+          description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex laudantium laborum error nihil quia quidem saepe dicta atque temporibus, aut quam recusandae veniam dignissimos modi, voluptatum deleniti hic necessitatibus consequatur.",
+          url:"https://picsum.photos/200/300"
+      },
+      {
+        name:"Foto 2",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex laudantium laborum error nihil quia quidem saepe dicta atque temporibus, aut quam recusandae veniam dignissimos modi, voluptatum deleniti hic necessitatibus consequatur.",
+        url:"https://picsum.photos/id/240/200/300"
+      },
+      {
+        name:"Foto 3",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex laudantium laborum error nihil quia quidem saepe dicta atque temporibus, aut quam recusandae veniam dignissimos modi, voluptatum deleniti hic necessitatibus consequatur.",
+        url:"https://picsum.photos/200/300/?blur=2"
+      }
+    ]
+  }
 
 }
